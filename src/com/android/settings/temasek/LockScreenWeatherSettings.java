b8d8129d49b58a999fff0bfae154f53628ce2d00
chickenfactory <micah.chickenfactory@gmail.com>
2015-05-23 11:18:05 -1000

    change reset dialog string for lock screen weather

--- src/com/android/settings/temasek/LockScreenWeatherSettings.java
+++ src/com/android/settings/temasek/LockScreenWeatherSettings.java
@@ -275,7 +275,7 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
                     .setTitle(R.string.reset)
                     .setMessage(R.string.dlg_reset_values_message)
                     .setNegativeButton(R.string.cancel, null)
-                    .setPositiveButton(R.string.dlg_reset_android,
+                    .setPositiveButton(R.string.ok,
                         new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             Settings.System.putInt(getOwner().mResolver,
