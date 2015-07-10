1f0fa5d1a852b2cb2d43ed5428f393414d148326
chickenfactory <micah.chickenfactory@gmail.com>
2015-07-10 05:33:51 -1000

    fix drawable in commit commit: https://github.com/mtshima/android_packages_apps_Settings/commit/ddbc936665baf0bcb2ca018ff6db4d719718c9f9
    Change-Id: I588a3467a718adc9bacb794976bbf90399922696

--- src/com/android/settings/temasek/LockScreenColorSettings.java
+++ src/com/android/settings/temasek/LockScreenColorSettings.java
@@ -98,7 +98,7 @@ public class LockScreenColorSettings extends SettingsPreferenceFragment implemen
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.add(0, MENU_RESET, 0, R.string.reset)
-                .setIcon(R.drawable.ic_menu_reset) // use the KitKat backup icon
+                .setIcon(R.drawable.ic_settings_reset) // use the KitKat backup icon
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
     }
 
